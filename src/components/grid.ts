Topdev97/o-spreadsheet

commit 8a9217a8688c9f32439388b200cea54aa9ba788a

[FIX] grid: Capture context menu events triggered by keyboard
Currently, we handle the contextmenu events by capturing the event on
the canvas. This made complete sense when the canvas was the default
focused element in the DOM but its no longer the case since 19a7ccf1.

The current code would still work because when clicking on the
contextmenu mouse button, the DOM will first dispatch a focus event on
the canvas and dispatch the context menu event afterwards.

However, a user can trigger a contextmenu event with his keyboard (on
non retard proprietary keyboards, hit "Menu key" or Shift+F10). With the
change introduced in 19a7ccf1, the event will not be captured by the
canvas since there wasn't a click to focus it beforehand and we end up
displaying the default browser context menu.

This commit introduced a dedicated callback for this situation that will
display the contextmenu under the top left cell of the current
selection.

Task 3093099

closes odoo/o-spreadsheet#1864

Signed-off-by: Lucas Lefèvre (lul) <lul@odoo.com>

src/components/grid.ts
@@ -180,7 +180,11 @@ const TEMPLATE = xml/* xml */ `
         />
     </t>
     <t else="1">
-      <input class="position-absolute" style="z-index:-1000;" t-on-input="onInput" t-ref="hiddenInput"/>
+      <input class="position-absolute"
+        style="z-index:-1000;"
+        t-on-input="onInput"
+        t-on-contextmenu="onInputContextMenu"
+        t-ref="hiddenInput"/>
     </t>
     <canvas t-ref="canvas"
       t-on-mousedown="onMouseDown"
@@ -863,6 +867,25 @@ export class Grid extends Component<Props, SpreadsheetEnv> {
   // ---------------------------------------------------------------------------
   // Context Menu
   // ---------------------------------------------------------------------------
+
+  onInputContextMenu(ev: MouseEvent) {
+    ev.preventDefault();
+    const lastZone = this.getters.getSelectedZone();
+    const { left: col, top: row } = lastZone;
+    let type: ContextMenuType = "CELL";
+    this.dispatch("STOP_EDITION");
+    if (this.getters.getActiveCols().has(col)) {
+      type = "COL";
+    } else if (this.getters.getActiveRows().has(row)) {
+      type = "ROW";
+    }
+    const [x, y, width, height] = this.getters.getRect(
+      lastZone,
+      this.getters.getActiveSnappedViewport()
+    );
+
+    this.toggleContextMenu(type, x + width, y + height);
+  }
 
   onCanvasContextMenu(ev: MouseEvent) {
     ev.preventDefault();

tests/components/grid.test.ts
@@ -597,6 +597,15 @@ describe("Grid component", () => {
       expect(fixture.querySelector(".o-menu div[data-name='add_row_before']")).toBeFalsy();
       expect(document.activeElement).toBe(fixture.querySelector(".o-grid>input"));
     });
+
+    test("Can open context menu with a keyboard input ", async () => {
+      const selector = ".o-grid>input";
+      const target = document.querySelector(selector)! as HTMLElement;
+      target.focus();
+      triggerMouseEvent(selector, "contextmenu", 0, 0, { button: 1, bubbles: true });
+      await nextTick();
+      expect(fixture.querySelector(".o-menu")).toBeTruthy();
+    });
   });
 });
 

commit 120b2eb43a4c7bbd94335441368cfd5c36649ea8

[FIX] grid: typo in component template
Commit 8a9217a8 introduced a nex input to partially support IME inputs
with a typo in the template, making the preventing the input to be
properly cleared when editing cell content.

src/components/grid.ts
@@ -178,7 +178,7 @@ const TEMPLATE = xml/* xml */ `
         focus="props.focusComposer"
         />
     </t>
-    <t else="1">
+    <t t-else="1">
       <input class="position-absolute"
         style="z-index:-1000;"
         t-on-input="onInput"